454ae43743b6ecc9520c7fce5596eceb1e7163cf
xiaotiao <xiaotiao@followme-inc.com>
2019-03-18 15:43:56 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-      testCHANGELOG
+      testCHANGELOG1
     </div>
     <router-view/>
   </div>
